91b02289ddc563e31bf0547202f71ba6f190162f
diegocamomila <diego.camomila@gmail.com>
2023-01-18 14:08:16 -0300

    FEAT: Criará o primeiro caso de uso referente a enviar uma notificaçao

---
+++ src/application/use-case/send-notification.ts
@@ -0,0 +1,29 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+
+interface SendNtificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  async execute(
+    request: SendNtificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+    return {
+      notification,
+    };
+  }
+}
