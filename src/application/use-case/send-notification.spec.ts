837f46d993a73e39ef1ba3710a22adbd0d729d8a
diegocamomila <diego.camomila@gmail.com>
2023-01-19 08:13:40 -0300

    FIX: Corrigirá o arquivo send-notification.spec.tss, referente ao test send-notification

--- src/application/use-case/send-notification.spec.ts
+++ src/application/use-case/send-notification.spec.ts
@@ -1,8 +1,15 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notificationRepository = {
+  async creat(notification: Notification) {
+    console.log(notification);
+  },
+};
 
 describe('SEnd notification', () => {
   it('shoud be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
