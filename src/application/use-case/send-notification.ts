3bf36faf37e110604ca40c96b62bdf1e0f161cf6
diegocamomila <diego.camomila@gmail.com>
2023-01-19 14:23:25 -0300

    Feat: Inplementará a classe cancel e algumas correçoes

--- src/application/use-case/send-notification.ts
+++ src/application/use-case/send-notification.ts
@@ -3,7 +3,7 @@ import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
 import { NotificationsRepository } from '../repositories/notifications-repositores';
 
-interface SendNtificationRequest {
+interface SendNotificationRequest {
   recipientId: string;
   content: string;
   category: string;
@@ -18,7 +18,7 @@ export class SendNotification {
   constructor(private notificationRepository: NotificationsRepository) {}
 
   async execute(
-    request: SendNtificationRequest,
+    request: SendNotificationRequest,
   ): Promise<SendNotificationResponse> {
     const { recipientId, content, category } = request;
 
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,7 +4,15 @@ import { NotificationsRepository } from 'src/application/repositories/notificati
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
