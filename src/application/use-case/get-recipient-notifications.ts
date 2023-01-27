5f3f47cc60b9d78976b85756b12e6abc5931aea2
diegocamomila <diego.camomila@gmail.com>
2023-01-27 10:32:15 -0300

    FEAT: get-recipient-notifications e get-recipient-notifications.spec

---
+++ src/application/use-case/get-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Notification } from '@application/entities/notification';
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface GetRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+  async execute(
+    request: GetRecipientNotificationRequest,
+  ): Promise<GetRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const notifications = await this.notificationRepository.findManyRecipientId(
+      recipientId,
+    );
+
+    return {
+      notifications,
+    };
+  }
+}
