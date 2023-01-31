6fb9fb59ed8443851482c5a83266e5dcdccc20ad
diegocamomila <diego.camomila@gmail.com>
2023-01-31 13:58:54 -0300

    FIX: Inplementando controlers no http. modules que faltou no providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,9 @@
 import { CancelNotification } from '@application/use-case/cancel-notification';
 import { CountRecipientNotification } from '@application/use-case/count-recipient-notifications';
+import { GetRecipientNotification } from '@application/use-case/get-recipient-notifications';
+import { ReadNotification } from '@application/use-case/read-notification';
 import { SendNotification } from '@application/use-case/send-notification';
+import { UnReadNotification } from '@application/use-case/unread-notification';
 import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notification.controller';
@@ -8,6 +11,13 @@ import { NotificationsController } from './controllers/notification.controller';
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification, CancelNotification, CountRecipientNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    ReadNotification,
+    UnReadNotification,
+    GetRecipientNotification,
+  ],
 })
 export class HttpModule {}
